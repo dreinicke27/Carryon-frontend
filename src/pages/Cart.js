f1cc2ae851cb026cb064a4614191da683f1adf21
dreinicke27 <danicareinicke@gmail.com>
2023-07-17 11:24:50 -0700

    Started to lay out cart page form, and home page carousel

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,121 @@
 const Cart = () => {
     return (
       <div className="container py-5">
-      <h1>Cart</h1>
+      <h1>CART</h1>
+      <form className="row g-3 py-5">
+        <h4>Shipping Information</h4>
+        <div className="col-md-6">
+          <label htmlFor="inputName" className="form-label">Name</label>
+          <input type="text" className="form-control" id="inputName"/>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputEmail4" className="form-label">Email</label>
+          <input type="email" className="form-control" id="inputEmail4"/>
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">Address</label>
+          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputCity" className="form-label">City</label>
+          <input type="text" className="form-control" id="inputCity"/>
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="inputState" className="form-label">State</label>
+          <select id="inputState" className="form-select">
+            <option selected>Choose...</option>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+          </select>
+        </div>
+        <div className="col-md-2">
+          <label htmlFor="inputZip" className="form-label">Zip</label>
+          <input type="text" className="form-control" id="inputZip"/>
+        </div>
+
+        <h4 className='pt-5'>Billing Information</h4>
+        <div className="col-md-6">
+          <label htmlFor="inputNameCard" className="form-label">Name On Card</label>
+          <input type="text" className="form-control" id="inputNameCard"/>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputCardNum" className="form-label">Card Number</label>
+          <input type="text" className="form-control" id="inputCardNum"/>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputExpDate" className="form-label">Expiration Date</label>
+          <input type="date" className="form-control" id="inputExpDate"/>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputCVV" className="form-label">CVV</label>
+          <input type="text" className="form-control" id="inputCVV"/>
+        </div>
+
+        <div className="col-12">
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="gridCheck"/>
+            <label className="form-check-label" htmlFor="gridCheck">
+              Different billing address
+            </label>
+          </div>
+        </div>
+        <div className="col-12">
+          <button type="submit" className="btn btn-dark">Checkout</button>
+        </div>
+        </form>
       </div>
       )};
   
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,42 @@
+import carmen from '../assets/img/carousel/carmen.png';
+import cole from '../assets/img/carousel/cole.png';
+import danica from '../assets/img/carousel/danica.png';
+import logo from '../assets/img/logoIcon.png';
+import carryOn from '../assets/img/carryOn.png';
+
 const Home = () => {
   return (
     <div className="container py-5">
-    <h1>Home</h1>
+      <div className='d-flex flex-row justify-content-around py-5'>
+        <img src={logo} alt="Carry On logo"></img>
+        <img src={carryOn} alt="Carry On"></img>
+      </div>
+      <div className='d-flex flex-row justify-content-center pb-5'>
+          <h3>Upcycled. Handmade. Sustainable Wears.</h3>
+      </div>
+      <div className='mx-auto col-md-6'>
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="3000">
+              <img src={cole} className="d-block w-100" alt="A man outside a coffee shop in a jacket made from a burlap green coffee bean sack"/>
+            </div>
+            <div className="carousel-item" data-bs-interval="3000">
+              <img src={carmen} className="d-block w-100" alt="A woman in a handmade jacket made out of a white curtain"/>
+            </div>
+            <div className="carousel-item" data-bs-interval="3000">
+              <img src={danica} className="d-block w-100" alt="A femme person in a handmade jacket made out of a colorful floral curtain"/>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </div>
     )};
   
